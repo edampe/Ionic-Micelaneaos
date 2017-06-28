12a4743bf3568d16cfe5868e41ed60f1ce3f32eb
Edilberto Amado P <edampe01@gmail.com>
2017-06-28 16:54:46 -0500

    Commit hasta el video 99 funcionando.

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { AjustesServices } from '../providers/ajustes/ajustes';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+//  rootPage:any = HomePage;
+  rootPage:any;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private _ajustes: AjustesServices) {
+    platform.ready().then(() => {
+
+      this._ajustes.cargar_storage().then( () => {
+
+        if ( this._ajustes.ajustes.mostrar_tutorial ){
+          this.rootPage = "IntroduccionPage"
+        }else{
+          this.rootPage = HomePage
+        }
+
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      } )
+
+
+
+
+    });
+  }
+}
+
---
+++ src/providers/ajustes/ajustes.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+// Plugins
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class AjustesServices {
+
+  ajustes = {
+    mostrar_tutorial: true 
+  }
+
+  constructor( private platform: Platform,
+               private storage: Storage ) {
+    console.log('Hello AjustesProvider Provider');
+  }
+
+  cargar_storage(){
+    
+    let promesa = new Promise( ( resolve, reject ) => {
+      if ( this.platform.is('cordova') ){
+
+              console.log('1');
+        
+
+          this.storage.ready()
+                  .then( () => {
+              console.log('2');
+                    
+            
+            this.storage.get('ajustes').then( ajustes => {
+
+              console.log('3');              
+
+              if( ajustes ){
+                  this.ajustes = ajustes
+                  console.log('Your age is', ajustes);
+              }
+
+              resolve()
+
+            });
+
+          })
+
+      }else{
+        console.log("Estamos en el comu")
+  
+        if ( localStorage.getItem('ajustes')){
+        console.log("entramos a los ajustes")
+          
+          this.ajustes = JSON.parse( localStorage.getItem('ajustes') )
+
+          resolve()
+        }
+      }
+    })
+
+    return promesa
+  }
+
+  guardar_storage(){
+
+    if ( this.platform.is('cordova') ){
+
+      console.log('4');      
+
+      this.storage.ready()
+                  .then( () => {
+
+              console.log('5');                    
+            
+            this.storage.set('ajustes', this.ajustes).then();
+
+          })
+
+    }else{
+      console.log("Estamos en el comu")      
+      localStorage.setItem('ajustes', JSON.stringify(this.ajustes))
+    }
+
+  }
+
+}
